471e136d5cd17d2e74875ca9fd0f95a6aeb2f2b9
Jonathan Scott Duff <duff@pobox.com>
2009-05-26 13:27:16 -0500

    created by proto's create-new-project

---
+++ lib/Example/Hello.pm
@@ -0,0 +1,11 @@
+class Example::Hello
+{
+    method greet { return "hello" }
+    method place { return "world" }
+}
+=begin pod
+=head1 NAME
+Example::Hello - canonical "hello, world!" example for proto
+=head1 AUTHOR
+perlpilot (perlpilot at github and @example.com)
+=end pod
